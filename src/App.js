8825cd03ec28c9b9d2848a45e1c78ebaffd5b239
Celeste Lewis <cdlewis42@gmail.com>
2020-05-11 17:48:33 -0400

    added more villager thumbnails

--- src/App.js
+++ src/App.js
@@ -9,7 +9,24 @@ function App() {
       <Header />
       <Navbar />
       <Card src = "https://i.pinimg.com/originals/ba/47/1b/ba471ba9e1a6af44147a63af95ca6f20.png"></Card>
+
       <Card src = "https://vignette.wikia.nocookie.net/videogames-fanon/images/e/ed/Coco_-_New_Leaf.png/revision/latest/scale-to-width-down/340?cb=20130813003417"></Card>
+
+      <Card src = "https://vignette.wikia.nocookie.net/animalcrossing/images/c/cc/Dotty_NH.png/revision/latest/top-crop/width/360/height/450?cb=20200321142147"></Card>
+
+      <Card src = "https://dodo.ac/np/images/thumb/2/25/Marina_NH.png/175px-Marina_NH.png"></Card>
+
+      <Card src = "https://dodo.ac/np/images/thumb/3/3a/Ozzie_NH.png/200px-Ozzie_NH.png"></Card>
+
+      <Card src = "https://vignette.wikia.nocookie.net/animalcrossing/images/7/7e/Ankha_NLa.png/revision/latest/top-crop/width/360/height/450?cb=20200413172251"></Card>
+
+      <Card src = "https://vignette.wikia.nocookie.net/animalcrossing/images/f/f7/Scoot_NewLeaf_Official.png/revision/latest/top-crop/width/360/height/450?cb=20130721211852"></Card>
+
+      <Card src = "https://dodo.ac/np/images/thumb/e/ec/Kabuki_NLa.png/175px-Kabuki_NLa.png"></Card>
+
+      <Card src = "https://vignette.wikia.nocookie.net/animalcrossing/images/3/32/Raddle_HD.png/revision/latest/top-crop/width/360/height/450?cb=20180518162935"></Card>
+
+      <Card src = "https://vignette.wikia.nocookie.net/animalcrossing/images/e/ef/Ribbot_NewLeaf_Official.png/revision/latest/top-crop/width/360/height/450?cb=20130818042038"></Card>
     </div>
   );
 }
