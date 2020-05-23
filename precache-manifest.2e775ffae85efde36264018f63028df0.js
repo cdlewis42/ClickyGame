308c3f32be3a6c0dac16b31996cd0119b896a4e7
Celeste Lewis <cdlewis42@gmail.com>
2020-05-23 18:39:29 -0400

    Updates

---
+++ precache-manifest.2e775ffae85efde36264018f63028df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8c38ef389b3452547b66d6e81a14d4",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "706b1b93897d756747d8",
+    "url": "/ClickyGame/static/css/main.81effb55.chunk.css"
+  },
+  {
+    "revision": "9f17989df6d52d0c10ed",
+    "url": "/ClickyGame/static/js/2.0ca85c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.0ca85c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706b1b93897d756747d8",
+    "url": "/ClickyGame/static/js/main.ef58d5d6.chunk.js"
+  },
+  {
+    "revision": "883b763d82ab0e0c736f",
+    "url": "/ClickyGame/static/js/runtime-main.6e0552f1.js"
+  },
+  {
+    "revision": "3947365774a757c4f01e7bd0d52649d8",
+    "url": "/ClickyGame/static/media/leafs.39473657.jpg"
+  }
+]);
