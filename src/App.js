2269edc96031de1e2d14c02a87da445aea864958
Celeste Lewis <cdlewis42@gmail.com>
2020-05-23 15:09:27 -0400

    changed mutating directly to setState

--- src/App.js
+++ src/App.js
@@ -11,43 +11,47 @@ class App extends Component{
     score: 0,
     alertMessage:""
   }
-
-  
+ 
 handleClick = () => {
   //when image is clicked, add 1 to score
   console.log("the click is working")
   console.log(this.state.score)
-  this.state.score ++
+  //if id doesnt equal id in pickedvills
+  //for
+  //if(this.id != )
+  const newState = this.state.score + 1
+  
+    this.setState({score: newState}) 
 
   }
 
   // shuffleVillagers = () =>{
   //   this.setState(this.state.villagers = this.shuffleArray(this.state.villagers))
-  // }
+  //  }
 
-  // shuffleArray = (a) =>{
-  //   var j,x,i;
-  //   for (i=a.length-1; i>0;i--){
-  //     j=Math.floor(Math.random()*(i+1))
-  //     x=a[i]
-  //     a[i]=a[j]
-  //     a[j] = x
-  //   }
-  //   return a
-  // }
-  // checkGuess = (name, cb) => {
-  //   const newState = { ...this.state };
-  //   if (newState.pickedChars.includes(name)) {
-  //     newState.alertMessage = `You already picked "${name.toUpperCase()}"!`
-  //     newState.pickedChars = []
-  //     this.setState(this.state = newState)
-  //   } else {
-  //     newState.pickedChars.push(name)
-  //     newState.alertMessage = "Correct!"
-  //     this.setState(this.state = newState)
-  //   }
-  //   cb(newState, this.alertWinner)
-  // }
+  //  shuffleArray = (a) =>{
+  //    var j,x,i;
+  //    for (i=a.length-1; i>0;i--){
+  //      j=Math.floor(Math.random()*(i+1))
+  //      x=a[i]
+  //      a[i]=a[j]
+  //      a[j] = x
+  //    }
+  //    return a
+  //  }
+  //  checkGuess = (name, cb) => {
+  //    const newState = { ...this.state };
+  //    if (newState.pickedChars.includes(name)) {
+  //      newState.alertMessage = `You already picked "${name.toUpperCase()}"!`
+  //      newState.pickedChars = []
+  //      this.setState(this.state = newState)
+  //    } else {
+  //      newState.pickedChars.push(name)
+  //      newState.alertMessage = "Correct!"
+  //      this.setState(this.state = newState)
+  //    }
+  //    cb(newState, this.alertWinner)
+  //  }
 
   // updateTopScore = (newState, cb) => {
   //   if (newState.pickedChars.length > newState.topScore) {
@@ -65,6 +69,7 @@ handleClick = () => {
         {this.state.villagers.map(villager=>(
           <Card
           id = {villager.id}
+          key = {villager.id}
           name = {villager.name}
           image = {villager.image}
           handleClick = {this.handleClick}
